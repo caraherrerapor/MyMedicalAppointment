52f45c9de9bbb888b7c4ae4dbdaf43a443304589
carlos herrera <caraherrerapor@gmail.com>
2020-11-04 20:40:05 -0500

    creacion primeras clases

---
+++ src/mymedicalappointments/Doctor.java
@@ -0,0 +1,28 @@
+
+package mymedicalappointments;
+
+
+public class Doctor {
+    static int id = 0; // Autoincrement
+    String name;
+    String speciality;
+    
+    Doctor(){
+        System.out.println("construyendo el objeto Doctor");
+        id++;
+    }
+    Doctor(String name){
+        System.out.println("El nombre del Doctor es "+ name);
+    }
+    
+
+    // behavior
+    
+    public void showName(){
+        System.out.println(name);
+    }
+    public void showId(){
+        System.out.println("ID Doctor "+ id);
+    }
+    
+}
---
+++ src/mymedicalappointments/Main.java
@@ -0,0 +1,21 @@
+
+package mymedicalappointments;
+
+import java.util.Scanner;
+import static mymedicalappointments.UIMenu.*;
+;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+      
+        
+        showMenu();
+       
+    }
+     
+}
+
+    
+
